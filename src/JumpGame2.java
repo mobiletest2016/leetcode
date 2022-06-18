b59ba353ef4de00781a118505ac50561e2b41383
mobiletest2016 <mobiletest.mt2016@gmail.com>
2022-06-18 18:09:40 +0530

    https://leetcode.com/problems/jump-game-ii/
    Signed-off-by: mobiletest2016 <mobiletest.mt2016@gmail.com>

---
+++ src/JumpGame2.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/jump-game-ii/
+
+public class JumpGame2 {
+    public int jump(int[] nums) {
+        int jumps = 0, curEnd = 0, curMax = 0;
+        for (int i = 0; i < nums.length - 1 && curEnd < nums.length - 1; i++) {
+            curMax = Math.max(curMax, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curMax;
+            }
+        }
+        return jumps;
+    }
+}
+
