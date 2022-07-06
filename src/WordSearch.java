d030dba071fbbb0a796017ca4ae3dc4decdb3f69
mobiletest2016 <mobiletest.mt2016@gmail.com>
2022-07-06 14:41:14 +0530

    https://leetcode.com/problems/word-search/
    More optimal backtracking
    
    Signed-off-by: mobiletest2016 <mobiletest.mt2016@gmail.com>

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,52 +1,33 @@
 // https://leetcode.com/problems/word-search/
+
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
                     int[][] lookup = new int[board.length][board[0].length];
-                    lookup[i][j] = 1;
-                    if (check(board, word, 0, i, j, lookup))
+                    if (exist(board, word, lookup, 0, i, j))
                         return true;
                 }
             }
         }
+
         return false;
     }
 
-    private boolean check(char[][] board, String word, int idx, int board_x, int board_y, int[][] lookup) {
-        if (idx == word.length()-1 && board[board_x][board_y] == word.charAt(idx))
+    private boolean exist(char[][] board, String word, int[][] lookup, int idx, int i, int j) {
+        if (idx == word.length())
             return true;
 
-        if (board_x + 1 < board.length && lookup[board_x + 1][board_y] == 0 && board[board_x + 1][board_y] == word.charAt(idx + 1)) {
-            lookup[board_x + 1][board_y] = idx + 1;
-            if (check(board, word, idx + 1, board_x + 1, board_y, lookup))
-                return true;
-            lookup[board_x + 1][board_y] = 0;
-        }
+        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || word.charAt(idx) != board[i][j] || lookup[i][j] == 1)
+            return false;
 
-        if (board_x - 1 >= 0 && lookup[board_x - 1][board_y] == 0 && board[board_x - 1][board_y] == word.charAt(idx + 1)) {
-            lookup[board_x - 1][board_y] = idx + 1;
-            if (check(board, word, idx + 1, board_x - 1, board_y, lookup))
-                return true;
-            lookup[board_x - 1][board_y] = 0;
-        }
+        lookup[i][j] = 1;
+        if (exist(board, word, lookup, idx + 1, i + 1, j) || exist(board, word, lookup, idx + 1, i - 1, j) ||
+                exist(board, word, lookup, idx + 1, i, j + 1) || exist(board, word, lookup, idx + 1, i, j - 1))
+            return true;
 
-        if (board_y + 1 < board[board_x].length && lookup[board_x][board_y + 1] == 0 && board[board_x][board_y + 1] == word.charAt(idx + 1)) {
-            lookup[board_x][board_y + 1] = idx + 1;
-            if (check(board, word, idx + 1, board_x, board_y + 1, lookup))
-                return true;
-            lookup[board_x][board_y + 1] = 0;
-        }
-
-        if (board_y - 1 >= 0 && lookup[board_x][board_y - 1] == 0 && board[board_x][board_y - 1] == word.charAt(idx + 1)) {
-            lookup[board_x][board_y - 1] = idx + 1;
-            if (check(board, word, idx + 1, board_x, board_y - 1, lookup))
-                return true;
-            lookup[board_x][board_y - 1] = 0;
-        }
-
+        lookup[i][j] = 0;
         return false;
-
     }
 }
